ae2311ce2bc3dbb96e75c21e4e5fdb9919451808
Megan Hepner <megan.hepner@gmail.com>
2020-06-01 11:49:56 -0700

    added in fuctions for sub, mult, and div

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,29 @@ const add = function(number1, number2) {
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
+const result = add(number1, number2);
+alert(result);
 
-alert(add(number1, number2));
+//replicates above, have to manually change the  const result between math terms//
+
+const add = function(number1, number2) {
+  return number1 + number2;
+};
+
+const subtract = function(number1, number2) {
+	return number1 - number2;
+};
+
+const multiply = function(number1, number2) {
+	return number1 * number2;
+};
+
+const divide = function(number1, number2) {
+	return number1 / number2;
+};
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+const result = divide(number1, number2);
+alert(result);
