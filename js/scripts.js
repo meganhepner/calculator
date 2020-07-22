5e4127a1e4c492365c30f9e7044ed369b96fff01
Megan Hepner <megan.hepner@gmail.com>
2020-07-21 19:46:29 -0700

    clean up comments and README

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,7 @@
-const add = function(number1, number2) {
-  return number1 + number2;
-};
-
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 const result = add(number1, number2);
 alert(result);
-
-//replicates above, have to manually change the  const result between math terms//
 
 const add = function(number1, number2) {
   return number1 + number2;
